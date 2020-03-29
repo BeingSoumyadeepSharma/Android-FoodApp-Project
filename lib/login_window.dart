29eaf3f024787ff2dc7c04621bad06fefa107a04
Soumyadeep Sharma <soumyadeepsharma99@gmail.com>
2020-03-30 00:45:08 +0530

    Login Widget Added

---
+++ lib/login_window.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginWin extends StatefulWidget {
+  @override
+  _LoginWinState createState() => _LoginWinState();
+}
+
+class _LoginWinState extends State<LoginWin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
