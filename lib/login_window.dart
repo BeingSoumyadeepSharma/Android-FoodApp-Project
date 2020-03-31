682a16718b150c16a9b9d7fdc0e71eabd2952b63
Soumyadeep Sharma <soumyadeepsharma99@gmail.com>
2020-04-01 00:34:13 +0530

    :sparkles: Login Page Designed only animation and Routing left :rocket:

--- lib/login_window.dart
+++ lib/login_window.dart
@@ -19,18 +19,154 @@ class _LoginWinState extends State<LoginWin> {
               colors: [Colors.white, Colors.red[900]],
             ),
           ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 339.5,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/foodpic.png'),
-                    fit: BoxFit.fill,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 339.5,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/foodpic.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: Align(
+                            alignment: Alignment(-0.5, 0),
+                            child: Text(
+                              "Hello Food!!",
+                              style: TextStyle(
+                                color: Colors.white,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(3.0, 3.0),
+                                    blurRadius: 5.0,
+                                    color: Colors.black54,
+                                  )
+                                ],
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black38,
+                                blurRadius: 20.0,
+                                offset: Offset(0, 5)),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey[100],
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                cursorColor: Colors.grey[100],
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Email or Phone number",
+                                  hintStyle: TextStyle(
+                                    fontSize: 17,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.grey[100],
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                cursorColor: Colors.grey[100],
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                    fontSize: 17,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 10.0,
+                                offset: Offset(0, 8)),
+                          ],
+                          gradient: LinearGradient(colors: [
+                            Color.fromRGBO(114, 0, 0, 1),
+                            Color.fromRGBO(165, 0, 0, 1),
+                            Color.fromRGBO(191, 0, 0, 1),
+                          ]),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
